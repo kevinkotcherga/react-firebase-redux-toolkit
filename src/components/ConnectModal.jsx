f5fa0d2c02e0460e2b75b8cfc17e09d8e681f8ed
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-11 15:06:43 +0200

    feat(components) : ajout des composants SignUp et Login dans ConnectModal

--- src/components/ConnectModal.jsx
+++ src/components/ConnectModal.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import Login from './Login';
+import SignUp from './SignUp';
 
 const ConnectModal = () => {
-  const [signUp, setSignUp] = useState(false);
+  const [signUp, setSignUp] = useState(true);
 
   return (
     <div className='connect-modal'>
       <div className="header-btn">
-        <button>S'inscrire</button>
-        <button>Se connecter</button>
+        <button onClick={() => setSignUp(true)} style={{background: signUp ? "rgb(28,28,28)" : "rgb(83,83,83"}}>S'inscrire</button>
+        <button onClick={() => setSignUp(false)} style={{background: signUp ? "rgb(83,83,83" : "rgb(28,28,28)"}}>Se connecter</button>
       </div>
+      {signUp ? <SignUp /> : <Login />}
     </div>
   );
 };
